03f0e7cc88a4622a8f5584145760f2cb36f1e727
João Barros <joaovictorpiresdev@gmail.com>
2026-03-18 08:36:47 -0300

    chore : Adição do index.html e inserção no app.py

--- app.py
+++ app.py
@@ -2,19 +2,8 @@ from wsgiref.simple_server import make_server
 
 def web_application(environ, start_response):
     start_response("200 Ok", [('Content-Type', "text/html;charset=utf-8")])
-    html = b'''
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Document</title>
-                </head>
-                <body>
-                    Hello world!
-                </body>
-                </html>
-            '''
-    return [html]
+    with open("index.html", "r", encoding="utf-8") as file:
+        html = file.read()
+    return [html.encode("utf-8")]
 
 make_server('', 5000, web_application).serve_forever()
