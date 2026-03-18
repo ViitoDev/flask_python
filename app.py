8afd460921ab2c2c5c1d02bedb0431999f0c69e9
João Barros <joaovictorpiresdev@gmail.com>
2026-03-18 09:37:22 -0300

    feat: lendo um "database" e iterando com o html

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from wsgiref.simple_server import make_server
 
 def web_application(environ, start_response):
+    products = [
+        {'name' : "Notebook", 'value' : 300},
+        {'name' : "Smartphone", 'value' : 350},
+        {'name' : "Tv", 'value' : 500},
+        {'name' : "Computer", 'value' : 800}
+    ]
+
+    html_lines = ""
+    for product in products:
+        html_lines += "<li> {} - {} </li>".format(product['name'], product['value'])
+
     start_response("200 Ok", [('Content-Type', "text/html;charset=utf-8")])
+    
     with open("index.html", "r", encoding="utf-8") as file:
-        html = file.read()
-    return [html.encode("utf-8")]
+        tamplate_html = file.read()
+
+    html_final = tamplate_html.replace("{{PRODUCTS}}", html_lines)
+    return [html_final.encode("utf-8")]
 
 make_server('', 5000, web_application).serve_forever()
